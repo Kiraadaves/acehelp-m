1f5eed0fd1d3eb7eb87934ebc57de569770a0817
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2025-02-25 16:29:05 +0100

    add changed

--- src/components/jsoncard.tsx
+++ src/components/jsoncard.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-import Pagination from "./pagination";
 
-interface Card {
+export interface PostsProps {
   id: number;
   title: string;
   body: string;
 }
 
 interface JsonCardProps {
-  post: Card;
+  post: PostsProps;
 }
 
 const JsonCard = ({ post }: JsonCardProps) => {
   return (
-    <main className="flex-grow space-y-4">
-      <h1 className="text-[#B771E5] text-center text-2xl md:text-4xl font-bold">
-        View our Posts!
-      </h1>
-      <div className="bg-[#b771e52f] px-8 shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
-        JsonCard <Pagination />
+    <div className="bg-white shadow-lg  rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
+      <div className="p-6 space-y-4">
+        <h2 className="text-xl font-semibold mb-2 text-gray-800">
+          {post.title.length < 30
+            ? `${post.title}`
+            : `${post.title.slice(0, 30)}...`}
+        </h2>
+        <p className="text-gray-600">
+          {post.body.length < 150
+            ? `${post.body}`
+            : `${post.body.slice(0, 150)}...`}
+        </p>
+        <div>
+          <button className="bg-[#B771E5] text-white p-2 rounded-md">Read more</button>
+        </div>
       </div>
-    </main>
+    </div>
   );
 };
 
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,24 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import NavBar from "./nav";
 import JsonCard from "./jsoncard";
 import Pagination from "./pagination";
+import axios from "axios";
+import { debounce } from "lodash";
+interface PostsProps {
+  id: number;
+  title: string;
+  body: string;
+}
 
 const Posts = () => {
+  const [cards, setCards] = useState<PostsProps[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const onSearch = (term: string) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setCards(response.data))
+      .catch((error) => console.error("Error fetching posts:", error));
+  });
+
+  const filteredCards = useMemo(() => {
+    return cards.filter(
+      (card) =>
+        card.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        card.body.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [cards, searchTerm]);
+
+  const onSearch = debounce((term: string) => {
     console.log(term);
-  };
+    setSearchTerm(term);
+    setCurrentPage(1);
+  });
   return (
     <div className="min-h-screen flex flex-col gap-8 md:gap-16">
       <NavBar onSearch={onSearch} />
-      <main className="flex-grow space-y-4">
+      <main>
         <h1 className="text-[#B771E5] text-center text-2xl md:text-4xl font-bold">
           View our Posts!
         </h1>
-        <div className="bg-[#b771e52f] px-8 shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
-          JsonCard <Pagination />
+        <div className="bg-[#b771e531] p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 text-center">
+          {cards.map((card) => (
+            <JsonCard key={card.id} post={card} />
+          ))}
         </div>
+        <Pagination />
       </main>
+
       <footer className="bg-[#B771E5] text-white text-center py-4 mt-auto">
         copyright Chinwe Nwankwo
       </footer>
