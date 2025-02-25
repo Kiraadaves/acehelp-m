7e5333750e6721a401772d49fc01f4f8493d04ff
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2025-02-25 12:55:58 +0100

    add axios, files, icons

---
+++ src/components/jsoncard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JsonCard = () => {
+  return (
+    <div>JsonCard</div>
+  )
+}
+
+export default JsonCard
---
+++ src/components/posts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./nav";
+
+const Posts = () => {
+  return (
+    <div>
+      <NavBar />
+      Posts
+      <footer>copyright Chinwe Nwankwo</footer>
+    </div>
+  );
+};
+
+export default Posts;
