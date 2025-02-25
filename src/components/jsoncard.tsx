568093165f6158a218aebee5cc5a3e6b8e811745
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2025-02-25 23:52:09 +0100

    fix complete

--- src/components/jsoncard.tsx
+++ src/components/jsoncard.tsx
@@ -13,23 +13,25 @@ interface JsonCardProps {
 
 const JsonCard = ({ post, openModal }: JsonCardProps) => {
   return (
-    <div className="animate__animated animate__slideInUp bg-white shadow-lg pb-6 flex flex-col gap-4 rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
-      <div className="p-6 space-y-4">
-        <h2 className="text-xl font-extrabold mb-2 text-[#B771E5]">
-          {post.title.length < 30
-            ? `${post.title}`
-            : `${post.title.slice(0, 30)}...`}
-        </h2>
-        <p className="text-gray-600">
-          {post.body.length < 100
-            ? `${post.body}`
-            : `${post.body.slice(0, 100)}...`}
-        </p>
+    <div className="animate__animated animate__slideInUp flex flex-col gap-4 bg-white shadow-lg pb-6 rounded-lg overflow-hidden">
+      <div className="  transition-transform duration-300 ease-in-out transform hover:scale-105">
+        <div className="p-6 space-y-4">
+          <h2 className="text-xl font-extrabold mb-2 text-[#B771E5] pt-1">
+            {post.title.length < 30
+              ? `${post.title}`
+              : `${post.title.slice(0, 30)}...`}
+          </h2>
+          <p className="text-gray-600">
+            {post.body.length < 100
+              ? `${post.body}`
+              : `${post.body.slice(0, 100)}...`}
+          </p>
+        </div>
       </div>
       <div className="mt-auto">
         <button
           onClick={() => openModal(post)}
-          className="bg-[#B771E5] text-white p-2 rounded-md"
+          className="bg-[#B771E5] text-white p-2 rounded-md transition-transform duration-300 ease-in-out transform hover:scale-105"
         >
           Read more
         </button>
