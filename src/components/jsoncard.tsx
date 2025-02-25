9fbec7ebea4bce4b645bcc83e5a1fe66bcbb0626
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2025-02-25 13:30:27 +0100

    posts page

--- src/components/jsoncard.tsx
+++ src/components/jsoncard.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Pagination from "./pagination";
 
-const JsonCard = () => {
+interface Card {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface JsonCardProps {
+  post: Card;
+}
+
+const JsonCard = ({ post }: JsonCardProps) => {
   return (
-    <main className="flex-grow">
+    <main className="flex-grow space-y-4">
       <h1 className="text-[#B771E5] text-center text-2xl md:text-4xl font-bold">
         View our Posts!
       </h1>
-      JsonCard
-      <Pagination />
+      <div className="bg-[#b771e52f] px-8 shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
+        JsonCard <Pagination />
+      </div>
     </main>
   );
 };
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import NavBar from "./nav";
 import JsonCard from "./jsoncard";
+import Pagination from "./pagination";
 
 const Posts = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -10,7 +11,14 @@ const Posts = () => {
   return (
     <div className="min-h-screen flex flex-col gap-8 md:gap-16">
       <NavBar onSearch={onSearch} />
-      <JsonCard />
+      <main className="flex-grow space-y-4">
+        <h1 className="text-[#B771E5] text-center text-2xl md:text-4xl font-bold">
+          View our Posts!
+        </h1>
+        <div className="bg-[#b771e52f] px-8 shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
+          JsonCard <Pagination />
+        </div>
+      </main>
       <footer className="bg-[#B771E5] text-white text-center py-4 mt-auto">
         copyright Chinwe Nwankwo
       </footer>
